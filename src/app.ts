e84fda30d55744143a1fef26c016250826119133
marzia <marziamostafa@gmail.com>
2025-09-04 01:43:15 +0600

    setup ongoing

--- src/app.ts
+++ src/app.ts
@@ -1,11 +1,20 @@
-const express = require('express')
-const app = express()
-const port = 3000
+import express, {
+  type Application,
+  type Request,
+  type Response,
+} from "express";
 
-app.get('/', (req, res) => {
-    res.send('Hello World!')
-})
+import cors from "cors";
 
-app.listen(port, () => {
-    console.log(`Example app listening on port ${port}`)
-})
+const app: Application = express();
+const port = 3000;
+
+// parser
+app.use(express.json());
+app.use(cors());
+
+app.get("/", (req: Request, res: Response) => {
+  res.send("Hello World!");
+});
+
+export default app;
