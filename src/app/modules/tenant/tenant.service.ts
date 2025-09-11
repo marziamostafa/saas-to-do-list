34e53a465578666ba0b7477f250ebdf08fa46511
marzia <marziamostafa@gmail.com>
2025-09-11 11:36:47 +0600

    service created to create,block and unblock tenant

--- src/app/modules/tenant/tenant.service.ts
+++ src/app/modules/tenant/tenant.service.ts
@@ -1,5 +1,15 @@
+import { ITenant } from "./tenant.interface";
 import { TenantModel } from "./tenant.model";
 import { ObjectId } from "mongodb";
+
+
+// create tenant
+const createTenant = async (tenant: ITenant) => {
+  const result = await TenantModel.create(tenant);
+  return result;
+}
+
+
 
 // get all tenants
 const getAllTenants = async () => {
@@ -9,5 +19,35 @@ const getAllTenants = async () => {
 
 const blockTenant = async (id: string) => {
   const query = { _id: new ObjectId(id) };
-  //   const result
+  const result = await TenantModel.findByIdAndUpdate(query, { isActive: false },
+    {
+      new: true
+    })
+  return result;
 };
+
+const unblockTenant = async (id: string) => {
+  const query = { _id: new ObjectId(id) };
+  const result = await TenantModel.findByIdAndUpdate(query, { isActive: true },
+    {
+      new: true
+    }
+
+  )
+  return result;
+}
+
+const deleteTenant = async (id: string) => {
+  const query = { _id: new ObjectId(id) };
+  const result = await TenantModel.findByIdAndDelete(query);
+  return result;
+}
+
+
+export const TenantService = {
+  getAllTenants,
+  blockTenant,
+  unblockTenant,
+  deleteTenant,
+  createTenant,
+}
